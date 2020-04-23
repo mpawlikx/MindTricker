5d0282fc0379b766c213e12de3ace8adb3b6bd3f
maciek <sennymaciek@gmail.com>
2020-04-23 15:49:01 +0200

    dodano liczenie punktacji i zmieniono widocznośc przycisków w GameActivity

--- app/src/main/java/com/example/mindtricker/GameActivity.kt
+++ app/src/main/java/com/example/mindtricker/GameActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_game.*
 import java.security.SecureRandom
@@ -13,6 +14,8 @@ class GameActivity : AppCompatActivity() {
 
     private lateinit var colorNames: Array<String>
     private lateinit var colorValues: Array<Int>
+    private var points = 0
+    private var isCorrect: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +24,16 @@ class GameActivity : AppCompatActivity() {
 
         splashTimer()
 
-        gameText.setOnClickListener {
-//            Toast.makeText(this, SecureRandom().nextBoolean().toString(), Toast.LENGTH_SHORT).show()
-
-
+        correctButton.setOnClickListener {
+            correctButtonActivate()
         }
+        wrongButton.setOnClickListener {
+            wrongButtonActivate()
+        }
+
+        //  gameText.setOnClickListener {
+//            Toast.makeText(this, SecureRandom().nextBoolean().toString(), Toast.LENGTH_SHORT).show()
+        //  }
     }
 
     private fun splashTimer() {
@@ -38,6 +46,8 @@ class GameActivity : AppCompatActivity() {
             override fun onFinish() {
                 initTimer()
                 randomizeValues()
+                wrongButton.visibility= View.VISIBLE
+                correctButton.visibility= View.VISIBLE
             }
         }
         timer.start()
@@ -57,7 +67,9 @@ class GameActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
                 timeText.text = "0:0"
-                gameText.text=" oo "// todo komunikat o ilości punktów
+                gameText.text = " oo "// todo komunikat o ilości punktów
+                wrongButton.visibility= View.INVISIBLE
+                correctButton.visibility= View.INVISIBLE
             }
         }
         timer.start()
@@ -77,7 +89,8 @@ class GameActivity : AppCompatActivity() {
     }
 
     private fun randomizeValues() {
-        if (SecureRandom().nextBoolean()) {
+        isCorrect=SecureRandom().nextBoolean()
+        if (isCorrect) {
             val randomIndex = abs(SecureRandom().nextInt()) % 4
             setTextValues(colorNames[randomIndex], colorValues[randomIndex])
         } else {
@@ -96,6 +109,28 @@ class GameActivity : AppCompatActivity() {
     private fun setTextValues(text: String, color: Int) {
         gameText.text = text
         gameText.setTextColor(color)
+    }
+
+    private fun correctButtonActivate() {
+        if (isCorrect) {
+            points++
+            pointsText.text = "$points"
+        } else {
+            points -= 2
+            pointsText.text = "$points"
+        }
+        randomizeValues()
+    }
+
+    private fun wrongButtonActivate() {
+        if (isCorrect) {
+            points -= 2
+            pointsText.text = "$points"
+        } else {
+            points++
+            pointsText.text = "$points"
+        }
+        randomizeValues()
     }
 
 }
