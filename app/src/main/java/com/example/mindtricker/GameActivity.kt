294e09b19b11d2829c8c1807af945f242401f442
maciek <sennymaciek@gmail.com>
2020-05-08 21:46:09 +0200

    dodano textedit i zaczęto zmianę shared preferences do zapisu w rankingu, dodano przycisk save w game activity

--- app/src/main/java/com/example/mindtricker/GameActivity.kt
+++ app/src/main/java/com/example/mindtricker/GameActivity.kt
@@ -1,11 +1,15 @@
 package com.example.mindtricker
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.IntegerRes
 import kotlinx.android.synthetic.main.activity_game.*
 import java.security.SecureRandom
 import kotlin.math.abs
@@ -21,6 +25,20 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         initializeAvailableColors()
+
+
+        val sharedPreferences = getSharedPreferences("player_names",Context.MODE_PRIVATE)
+        saveButton.setOnClickListener {
+            val name= playerNameEditText.text.toString().trim()
+            val points = Integer.parseInt(pointsText.text.toString().trim())
+            val editor = sharedPreferences.edit()
+            editor.putString("NAME",name)
+            editor.putInt("POINTS", points)
+            editor.apply()
+            val aname = sharedPreferences.getString("NAME","")
+            val apoints = sharedPreferences.getInt("POINTS",0)
+            test_text.text="Name: $name\nPoints: $points"
+        }
 
         splashTimer()
 
@@ -67,9 +85,13 @@ class GameActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
                 timeText.text = "0:0"
-                gameText.text = " oo "// todo komunikat o ilości punktów
+                gameText.visibility= View.INVISIBLE // todo komunikat o ilości punktów
+                endGameText.visibility =View.VISIBLE
                 wrongButton.visibility= View.INVISIBLE
                 correctButton.visibility= View.INVISIBLE
+                playerNameEditText.visibility= View.VISIBLE
+                saveButton.visibility= View.VISIBLE
+
             }
         }
         timer.start()
@@ -121,6 +143,8 @@ class GameActivity : AppCompatActivity() {
         }
         randomizeValues()
     }
+
+
 
     private fun wrongButtonActivate() {
         if (isCorrect) {
